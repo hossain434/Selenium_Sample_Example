b4c6af817cb0e5833af164a979bcb197ac98072e
Md Hossain <arif434@gmail.com>
2020-06-21 21:52:23 -0700

    first commit

---
+++ src/main/java/selenium_sample_example_2/Selenium_sample_example_testng.java
@@ -0,0 +1,9 @@
+package selenium_sample_example_2;
+
+public class Selenium_sample_example_testng {
+
+	public Selenium_sample_example_testng() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/test/java/selenium_sample_example_2/Selenium_sample_example_test.java
@@ -0,0 +1,30 @@
+package selenium_sample_example_2;
+
+import org.testng.annotations.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+public class Selenium_sample_example_test {
+
+	//public static void main(String args[]) {
+	@Test
+	  public void verifyHomepageTitle() {
+		System.setProperty("webdriver.chrome.driver","chromedriver/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+		WebDriver driver = new ChromeDriver(options);
+		String baseUrl = "http://demo.guru99.com/test/newtours/";
+		String expectedTitle = "Welcome: Mercury Tours";
+		String actualTitle = "";
+		driver.get(baseUrl);
+		actualTitle = driver.getTitle();
+		if (actualTitle.contentEquals(expectedTitle)) {
+			System.out.println("Test Passed: Title is - " +actualTitle);
+		} else {
+			System.out.println("Test Failed" );
+		}
+		driver.close();
+	}
+}
