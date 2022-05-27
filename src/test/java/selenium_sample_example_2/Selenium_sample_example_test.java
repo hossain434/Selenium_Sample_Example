22a2f732ec0e23f7344734d9092d1ca70baea2db
Arif Hossain <arif434@gmail.com>
2022-05-27 17:54:05 -0400

    Update Selenium_sample_example_test.java

--- src/test/java/selenium_sample_example_2/Selenium_sample_example_test.java
+++ src/test/java/selenium_sample_example_2/Selenium_sample_example_test.java
@@ -11,22 +11,10 @@ public class Selenium_sample_example_test {
 	//public static void main(String args[]) {
 	@Test
 	  public void verifyHomepageTitle() {
-		//System.setProperty("webdriver.chrome.driver","chromedriver/chromedriver.exe");
-		System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("headless");
-		WebDriver driver = new ChromeDriver(options);
-		//WebDriver driver = new RemoteWebDriver(new URL("http://10.0.0.158:4444/wd/hub"), options);
-		String baseUrl = "http://demo.guru99.com/test/newtours/";
-		String expectedTitle = "Welcome: Mercury Tours";
-		String actualTitle = "";
-		driver.get(baseUrl);
-		actualTitle = driver.getTitle();
-		if (actualTitle.contentEquals(expectedTitle)) {
-			System.out.println("Test Passed: Title is - " +actualTitle);
-		} else {
-			System.out.println("Test Failed" );
-		}
-		driver.close();
+	  ChromeOptions chromeOptions = new ChromeOptions();
+      WebDriver driver = new RemoteWebDriver(new URL("http://10.0.0.158:4444/wd/hub"), chromeOptions);
+      driver.get("http://www.google.com");
 	}
-}
+	  driver.quit();
+	}
+	}
